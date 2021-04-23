6b47b86d1d15b4ee6796bce4a2fdb93c4822254f
wubowen2018 <wubowen2017@gmail.com>
2021-04-23 15:14:41 +0800

    简陋的登录页面

---
+++ src/components/home/Appindex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+      hello world!
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data () {
+    return {
+             
+    };
+  },
+  props: {
+      
+  },
+
+  components: {
+    
+  },
+  computed: {
+    
+  },
+  mounted() {
+    
+  },
+  beforeDestory() {
+    
+  },
+  methods: {
+    
+  },
+}
+
+</script>
+<style scoped>
+    
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/components/Login')
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: () => import('@/components/home/AppIndex')
+    }
+  ]
+})
