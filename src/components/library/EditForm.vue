e4efb009fd9210bee12514a142ddbdc6f80f9801
wubowen2018 <wubowen2017@gmail.com>
2021-05-31 11:49:16 +0800

    添加上传图片的代码

--- src/components/library/EditForm.vue
+++ src/components/library/EditForm.vue
@@ -21,7 +21,9 @@
         </el-form-item>
         <el-form-item label="封面" :label-width="formLabelWidth" prop="cover">
           <el-input v-model="form.cover" autocomplete="off" placeholder="图片 URL"></el-input>
+          <img-upload @onUpload="uploadImg" ref="imgUpload"></img-upload>
         </el-form-item>
+
         <el-form-item label="简介" :label-width="formLabelWidth" prop="abs">
           <el-input type="textarea" v-model="form.abs" autocomplete="off"></el-input>
         </el-form-item>
@@ -49,8 +51,10 @@
 
 <script>
 /*eslint-disable*/
+  import ImgUpload from './ImgUpload'
   export default {
     name: 'EditForm',
+    components:{ImgUpload},
     data () {
       return {
         dialogFormVisible: false,
@@ -100,7 +104,11 @@
             this.$emit('onSubmit')
           }
         })
+      },
+      uploadImg () {
+        this.form.cover = this.$refs.imgUpload.url
       }
+
     }
   }
 </script>
