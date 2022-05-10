d4063d2f58a228b0723b1db0965d4a3f36ebd0c8
wubowen2018 <wubowen2017@gmali.com>
2022-05-10 10:34:51 +0800

    添加二维码

--- src/components/home/Appindex.vue
+++ src/components/home/Appindex.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="wrapper">
-      hello world!
+  <div>
+      	<el-button type="primary" @click="qrCode()" style="margin-left: 20px">生成二维码</el-button>
+		<el-dialog title="支付" :visible.sync="dialogVisible" width="20%">
+            <div id="qrcode"></div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+	
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import QRCode from 'qrcodejs2'
 export default {
   data () {
     return {
-             
+        dialogVisible: false     
     };
   },
   props: {
@@ -29,7 +38,19 @@ export default {
     
   },
   methods: {
-    
+	qrCode() {
+		this.dialogVisible = true;
+		this.$nextTick(function () {
+			document.getElementById("qrcode").innerHTML = "";
+			let qrcode = new QRCode("qrcode", {
+				width: 150,
+				height: 150,
+				text: "https://www.jimmywu.top",
+				colorDark: "#109dff",
+				colorLight: "#d9d9d9"
+			});
+		});
+	}
   },
 }
 
